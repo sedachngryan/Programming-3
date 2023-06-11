dc80ead71ae53237302d3d12608cbf09bae786ef
sedachngryan <135506506+sedachngryan@users.noreply.github.com>
2023-06-11 13:14:46 +0400

    Add files via upload

--- classPredator.js
+++ classPredator.js
@@ -1,19 +1,8 @@
 
-class Predator {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+class Predator extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
         this.energy = 3;
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ]
     }
 
     getNewDirections() {
@@ -29,21 +18,9 @@ class Predator {
         ]
     }
 
-    chooseCell(character) {
-        this.getNewDirections()
-        var found = []
-        for (var i = 0; i < this.directions.length; i++) {
-            var x = this.directions[i][0]
-            var y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == character) {
-                    found.push(this.directions[i])
-                }
-            }
-
-        }
-        return found;
-
+    chooseCell(char) {
+        this.getNewCoordinates();
+        return super.chooseCell(char);
     }
 
     mult() {
