2f0915f06fa1b4cbf0fd4e8ec082583669e828f2
sedachngryan <135506506+sedachngryan@users.noreply.github.com>
2023-06-11 13:14:15 +0400

    Add files via upload

--- classEnergy.js
+++ classEnergy.js
@@ -1,11 +1,8 @@
-class Energy {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+class Energy extends LivingCreature {
+    constructor(x, y, index) {
+        super(x, y, index);
         this.count = 0
         this.energy = 10;
-        this.directions = [
-        ]
     }
 
     getNewDirections() {
@@ -22,68 +19,9 @@ class Energy {
     }
 
     chooseCell(char1, char2) {
-        this.getNewDirections()
-        var found = []
-        for (var i = 0; i < this.directions.length; i++) {
-            var x = this.directions[i][0]
-            var y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char1 || matrix[y][x] == char2) {
-                    found.push(this.directions[i])
-                }
-            }
-        }
-        return found
+        this.getNewCoordinates();
+        return super.chooseCell(char1, char2);
     }
-    // choosePredatorCell() {
-    //     this.getNewDirections()
-    //     var found = []
-    //     for (var y = 0; y < matrix.length; y++) {
-    //         for (var x = 0; x < matrix.length; x++)
-    //             if (matrix[y][x] == 3) {
-    //                 found.push(x, y)
-    //             }
-    //     }
-    //     return found
-    // }
-    // eat() {
-    //     var food = random(this.chooseCell(1, 3))
-    //     if (food) {
-    //         matrix[this.y][this.x] = 0
-    //         var newX = food[0]
-    //         var newY = food[1]
-    //         if (matrix[newY][newX] == 1) {
-    //             for (var i in grassArr) {
-    //                 if (grassArr[i].x == newX && grassArr[i].y == newY) {
-    //                     grassArr.splice(i, 4)
-    //                 }
-    //             }
-    //         }
-    //         if (matrix[newY][newX] == 3) {
-    //             for (var i in predatorArr) {
-    //                 if (predatorArr[i].x == newX && predatorArr[i].y == newY) {
-    //                     predatorArr[i].energy++;
-    //                     this.energy--
-    //                 }
-    //             }
-    //         }
-
-    //         matrix[newY][newX] = 4
-    //         this.x = newX
-    //         this.y = newY
-    //     }
-    //     this.count++;
-    //     if (this.count > 6) {
-    //         this.energy++
-    //         this.count = 0
-    //         for (var i in predatorArr) {
-    //             if (predatorArr[i].x == newX && predatorArr[i].y == newY) {
-    //                 predatorArr[i].energy++;
-    //                 this.energy--
-    //             }
-    //         }
-    //     }
-    // }
 
     eat() {
         var food = random(this.chooseCell(1))
